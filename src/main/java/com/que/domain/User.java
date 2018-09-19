c3195bf73a85ad74303c648cbaa6272d05790811
Jiang <422836331@qq.com>
2018-09-20 00:56:07 +0800

    20180920

---
+++ src/main/java/com/que/domain/User.java
@@ -0,0 +1,48 @@
+package com.que.domain;
+
+public class User {
+    public User(Integer id, String name, String age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    private Integer id;
+    private String name;
+    private String age;
+    private String address;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
